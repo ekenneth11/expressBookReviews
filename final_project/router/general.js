297940fc80a2524dff91e20c11302fafa3c4c0b4
Kenneth Bautista <kennethbautista1512@gmail.com>
2025-03-13 16:28:08 -0400

    updated the other requests

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,14 +45,29 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let keys = Object.keys(books);
+  let filtered_book = {};
+  keys.forEach((key) =>{
+    let book = books[key];
+    if (book.title == title){
+      filtered_book[key] = book;
+    }
+  })
+  // filtered_book = books.filter((book) => book.author === author);
+  if (Object.keys(filtered_book).length > 0){
+    return res.send(JSON.stringify(filtered_book, null, 4));
+  }
+  return res.status(300).json({message: "No authors found!"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if(books[isbn]){
+    return res.send(JSON.stringify(books[isbn].reviews, null, 4));
+  }
+  return res.status(404).json({message: "Book not found!"});
 });
 
 module.exports.general = public_users;
